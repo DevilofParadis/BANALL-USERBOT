1aa1060ce04c9ff6872013dfa8f5b12f2c9b5f09
Pandey Amit <114922080+DevilofParadis@users.noreply.github.com>
2024-05-06 01:49:16 +0530

    Update Oxygen.py

--- Oxygen.py
+++ Oxygen.py
@@ -5,7 +5,7 @@ from asyncio import sleep
 from os import execle, getenv, environ
 from pyrogram import Client, filters, idle, errors
 from pyrogram.types import Message
-from pyrogram.errors import Floodwait
+from pyrogram.errors import FloodWait
 from pyrogram.handlers import MessageHandler
 
 
